0b6873c422979eae2b39e9b6813aaacf59d15b1f
Vinícius de Freitas <viniciusfreitas.1.4.4@gmail.com>
2021-10-17 19:05:57 -0300

    feat: adding sidebar component, close button svg, context for sidebar movement

---
+++ src/providers/sidebar/index.js
@@ -0,0 +1,22 @@
+import { createContext, useState } from "react";
+
+export const SidebarContext = createContext()
+
+export const SidebarProvider = ({ children }) => {
+
+    const [isOpen, setIsOpen] = useState(false)
+
+    const openSidebar = () => {
+        setIsOpen(true)
+    }
+
+    const closeSidebar = () => {
+        setIsOpen(false)
+    }
+
+    return (
+        <SidebarContext.Provider value={{ isOpen, openSidebar, closeSidebar }} >
+            {children}
+        </SidebarContext.Provider>
+    )
+}
